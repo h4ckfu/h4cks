bfaa7ee2e24dbeb4b7b591a4f0dc41db8454666e
h4ckfu <h4ckfu@protonmail.com>
2018-12-09 16:30:48 -0500

    update readme, SimplestVideo

--- README.md
+++ README.md
@@ -4,6 +4,4 @@ stupid python h4cks
 Got to learn GitHub inside and out..  
 Do better, know more than just commit, it's 2018
 
-1.) Publish here
-2.) ...
-3.) Profit
+u_p_g=['publish_here', '...', 'profit']
--- SimplestWebCamCapture.py
+++ SimplestWebCamCapture.py
@@ -1,12 +1,17 @@
+# This is a very simple, minimal python script to capture video through
+# a webcam and save it to a hard drive. Use a filename to test w/o webcam.
+
 import cv2
 
-cap = cv2.VideoCapture(0) # need a webcam for this to work.  #replace filename
+cap = cv2.VideoCapture(0) # need a webcam for this to work or replace w/ filename
 
-width=int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-height=int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-# Your VideoWriter Codec may vary
-writer=cv2.VideoWriter('this_video.avi',cv2.VideoWriter_fourcc(*'XVID'),20,(width,height))
+# Your VideoWriter Codec may vary info here:
+# https://www.pyimagesearch.com/2016/02/22/writing-to-video-with-opencv/
+
+writer = cv2.VideoWriter('captured.avi', cv2.VideoWriter_fourcc(*'XVID'),20,(w,h))
 
 while True:
     ret,frame = cap.read()
