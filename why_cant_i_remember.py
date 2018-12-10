bd72cd9131dcd9d97aafd2bf2f5d913b8f9965c0
h4ckfu <h4ckfu@protonmail.com>
2018-12-10 13:08:57 -0500

    cleaned it up a little, g

--- why_cant_i_remember.py
+++ why_cant_i_remember.py
@@ -1,11 +1,11 @@
 #/home/h4ckfu/anaconda3/bin/python3
 #04.21.18
 # These are things that i've sadly looked up...
-# Just notes essentially 
+# Just notes essentially
 # https://www.youtube.com/watch?v=Z3_lIPESVTw
 
-test_string="Why Cant I Remember"
-s='ant'
+test_string="Seether - Same Damn Life"
+s='amn'
 
 last_two_chars = test_string[-2:] # Get the end of a string
 
@@ -13,8 +13,11 @@ new_list = test_string.split(" ") # make a new list from a string
 make_a_string = " ".join(new_list) # make a string from that new list
 
 print(f'\n \t the last two characters of {test_string} (test_string[-2:]) are {last_two_chars}')
-print(f'\n \t (test_string.split(" ")) makes a {type(new_list)} from test_string')
-print(f'\n \t and then (" ".join(new_list)) makes that new_list back into {type(make_a_string)}')
+print(f' \t (test_string.split(" ")) makes a {type(new_list)} from test_string')
+print(f' \t The first element of new_list is: {new_list[0]}')
+
+
+print(f'\n \t and then " ".join(new_list) makes that new_list back into {type(make_a_string)}')
 
 print(f'\n \t index {test_string.index(s)} and find {test_string.find(s)} are !=')
 
